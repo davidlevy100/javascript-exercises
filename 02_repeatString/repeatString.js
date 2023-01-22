ac5809e8779d079dd42bdf7fa38d1af623b43904
David Levy <david.levy@wwt.com>
2023-01-22 14:14:40 -0500

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
 
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    return text.repeat(num)
 };
 
 // Do not edit below this line
