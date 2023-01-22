512a315cd0a84f47d5a0424400a83a4af4aebd5a
David Levy <david.levy@wwt.com>
2023-01-22 14:41:27 -0500

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+
+    nums.forEach(element => {
+        let index = arr.indexOf(element)
+        if (index > -1) {
+            arr.splice(index, 1)
+        }
+    });
+
+    return arr
 
 };
 
